fa1b49daa0db5bdde429638a987409aaa46d4141
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-03-08 19:46:41 +0100

    simplification

--- Sources/plate/directories.swift
+++ Sources/plate/directories.swift
@@ -4,18 +4,14 @@ import Foundation
 //     case url, string
 // }
 
-public struct Directories {
+public struct Home {
     public init() {}
 
-    public struct Home {
-        public init() {}
+    public func url() -> URL {
+        return FileManager.default.homeDirectoryForCurrentUser
+    }
 
-        public func url() -> URL {
-            return FileManager.default.homeDirectoryForCurrentUser
-        }
-
-        public func string() -> String {
-            return "\(FileManager.default.homeDirectoryForCurrentUser)"
-        }
+    public func string() -> String {
+        return "\(FileManager.default.homeDirectoryForCurrentUser)"
     }
 }
