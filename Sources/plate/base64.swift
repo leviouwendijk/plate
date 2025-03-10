67da93962357ee3b0097bc7b37fe4ae4847401f3
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-03-10 23:07:15 +0100

    base64 protocol

---
+++ Sources/plate/base64.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+public protocol Base64Encodable {
+    func base64() -> String?
+}
+
+extension URL: Base64Encodable {
+    public func base64() -> String? {
+        do {
+            let fileData = try Data(contentsOf: self)
+            return fileData.base64EncodedString()
+        } catch {
+            print("Error reading file: \(error)".ansi(.red))
+            return nil
+        }
+    }
+}
+
+public protocol Base64Decodable {
+    func base64() -> Data?
+}
+
+extension String: Base64Decodable {
+    public func base64() -> Data? {
+        return Data(base64Encoded: self, options: .ignoreUnknownCharacters)
+    }
+}
