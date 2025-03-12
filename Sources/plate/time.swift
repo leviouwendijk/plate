43ece5d39762b26dcddb11f081d100c713d75e51
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-03-12 19:30:23 +0100

    fix to return a double expected by time add interval

--- Sources/plate/time.swift
+++ Sources/plate/time.swift
@@ -1,11 +1,17 @@
 import Foundation
 
 public protocol SecondConvertible {
-    func hoursToSeconds() -> Int
+    func hoursToSeconds() -> Double
 }
 
 extension Int: SecondConvertible {
-    public func hoursToSeconds() -> Int {
+    public func hoursToSeconds() -> Double {
+        return Double(3600) * Double(self)
+    }
+}
+
+extension Double: SecondConvertible {
+    public func hoursToSeconds() -> Double {
         return 3600 * self
     }
 }
