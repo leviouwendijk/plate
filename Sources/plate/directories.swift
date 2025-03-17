8fb329f407621717fe8985ecbe822f95e261474c
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-03-17 18:42:15 +0100

    User.string() added

--- Sources/plate/directories.swift
+++ Sources/plate/directories.swift
@@ -15,3 +15,26 @@ public struct Home {
         return FileManager.default.homeDirectoryForCurrentUser.path
     }
 }
+
+public struct User {
+    public init() {}
+
+    public static func string() -> String {
+        let process = Process()
+        let pipe = Pipe()
+
+        process.executableURL = URL(fileURLWithPath: "/usr/bin/whoami")
+        process.standardOutput = pipe
+
+        do {
+            try process.run()
+            process.waitUntilExit()
+
+            let data = pipe.fileHandleForReading.readDataToEndOfFile()
+            return String(data: data, encoding: .utf8)?.trimmingCharacters(in: .whitespacesAndNewlines) ?? "Unknown"
+        } catch {
+            print("Error getting current user: \(error)")
+            return ""
+        }
+    }
+}
