160eab5b37d19c787edf1bccbb873f123fd30935
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-03-24 15:49:47 +0100

    forgot defaulting obscure to 4 revealed chars

--- Sources/plate/obscure.swift
+++ Sources/plate/obscure.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public func obscure(_ string: String,_ revealed: Int,_ obscurechar: Character = "*") -> String {
+public func obscure(_ string: String,_ revealed: Int = 4,_ obscurechar: Character = "*") -> String {
     let charcount = string.count
 
     if charcount <= revealed {
