b2beecaba4b693d2fb88e204a920c8daa7a05fc7
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-03-24 18:54:37 +0100

    delegation of URLSession to main queue

--- Sources/plate/network-request.swift
+++ Sources/plate/network-request.swift
@@ -147,7 +147,7 @@ public final class NetworkRequestStream: NSObject, URLSessionDataDelegate, @unch
             request.addValue(value, forHTTPHeaderField: key)
         }
         
-        let session = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
+        let session = URLSession(configuration: .default, delegate: self, delegateQueue: OperationQueue.main)
         self.task = session.dataTask(with: request)
     }
     
