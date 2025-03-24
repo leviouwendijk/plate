3d66c541551bb69884277807d844e041e402be8e
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-03-24 18:07:18 +0100

    repeating char selection in align protocol

--- Sources/plate/align.swift
+++ Sources/plate/align.swift
@@ -6,20 +6,20 @@ public enum Alignment {
 }
 
 public protocol Alignable {
-    func align(_ side: Alignment,_ width: Int) -> String
+    func align(_ side: Alignment,_ width: Int,_ char: String) -> String
 }
 
 extension String: Alignable {
     // example use -> "This is a string".align(.left, 50)
-    public func align(_ side: Alignment,_ width: Int = 80) -> String {
+    public func align(_ side: Alignment,_ width: Int = 80,_ char: String = ".") -> String {
         let padding = max(0, width - self.count)
 
         switch side {
         case .left:
-            let paddedText = self + String(repeating: ".", count: padding)
+            let paddedText = self + String(repeating: char, count: padding)
             return paddedText
         case .right:
-            let paddedText = String(repeating: ".", count: padding) + self
+            let paddedText = String(repeating: char, count: padding) + self
             return paddedText
         }
     }
