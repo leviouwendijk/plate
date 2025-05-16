002b139145806d0a3941bbf383deb06d3f20d0ce
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-05-16 22:29:15 +0200

    expanding initializers in structs

--- Sources/plate/api/mailer/mailer-variables.swift
+++ Sources/plate/api/mailer/mailer-variables.swift
@@ -13,6 +13,32 @@ public struct MailerAPIInvoiceVariables: Encodable {
     public let total:         String
     public let terms_total:   String
     public let terms_current: String
+
+    public init(
+        clientName:    String,
+        email:         String,
+        invoiceId:     String,
+        dueDate:       String,
+        productLine:   String,
+        amount:        String,
+        vatPercentage: String,
+        vatAmount:     String,
+        total:         String,
+        termsTotal:    String,
+        termsCurrent:  String
+    ) {
+        self.client_name    = clientName
+        self.email          = email
+        self.invoice_id     = invoiceId
+        self.due_date       = dueDate
+        self.product_line   = productLine
+        self.amount         = amount
+        self.vat_percentage = vatPercentage
+        self.vat_amount     = vatAmount
+        self.total          = total
+        self.terms_total    = termsTotal
+        self.terms_current  = termsCurrent
+    }
 }
 
 // lead -- /confirmation, /follow, /check
@@ -22,30 +48,64 @@ public struct MailerAPILeadVariables: Encodable {
 
     // public let time_range: [String:[String:String]]?
     public let time_range: MailerAPIAvailabilityContent?
+
+    public init(
+        name:       String,
+        dog:        String,
+        availability schedules: [MailerAPIWeekday: MailerAPIDaySchedule]? = nil
+    ) {
+        self.name       = name
+        self.dog        = dog
+        if let schedules = schedules {
+            self.time_range = MailerAPIAvailabilityContent(from: schedules)
+        } else {
+            self.time_range = nil
+        }
+    }
 }
 
 // quote -- /issue, /follow
 public struct MailerAPIQuoteVariables: Encodable {
     public let name:       String
     public let dog:        String
+
+    public init(name: String, dog: String) {
+        self.name = name
+        self.dog  = dog
+    }
 }
 
 // affiliate -- /food
 public struct MailerAPIAffiliateVariables: Encodable {
     public let name:       String
     public let dog:        String
+
+    public init(name: String, dog: String) {
+        self.name = name
+        self.dog  = dog
+    }
 }
 
 // service -- /follow, /onboarding
 public struct MailerAPIServiceVariables: Encodable {
     public let name:       String
     public let dog:        String
+
+    public init(name: String, dog: String) {
+        self.name = name
+        self.dog  = dog
+    }
 }
 
 // resolution -- /review, /onboarding
 public struct MailerAPIResolutionVariables: Encodable {
     public let name:       String
     public let dog:        String
+
+    public init(name: String, dog: String) {
+        self.name = name
+        self.dog  = dog
+    }
 }
 
 // custom -- /message/send
@@ -53,12 +113,29 @@ public struct MailerAPICustomVariables: Encodable {
     public let body:       String
     // public let time_range: [String:[String:String]]?
     public let time_range: MailerAPIAvailabilityContent?
+
+    public init(
+        body:         String,
+        availability schedules: [MailerAPIWeekday: MailerAPIDaySchedule]? = nil
+    ) {
+        self.body = body
+        if let schedules = schedules {
+            self.time_range = MailerAPIAvailabilityContent(from: schedules)
+        } else {
+            self.time_range = nil
+        }
+    }
 }
 
 // custom -- /template/fetch
 public struct MailerAPITemplateVariables: Encodable {
     public let category:    String
     public let file:        String
+
+    public init(category: String, file: String) {
+        self.category = category
+        self.file     = file
+    }
 }
 
 
@@ -70,6 +147,16 @@ public struct MailerAPIAppointmentVariables: Encodable {
 
     // public let ics_files:    [String]?  // base64‐encoded ICS blobs
     // handle ICS in payload initializer?
+
+    public init(
+        name:         String,
+        dog:          String,
+        appointments: [MailerAPIAppointmentContent]
+    ) {
+        self.name         = name
+        self.dog          = dog
+        self.appointments = appointments
+    }
 }
 
 // content of appointment var 
@@ -81,6 +168,24 @@ public struct MailerAPIAppointmentContent: Encodable {
     public let number:   String
     public let area:     String
     public let location: String
+
+    public init(
+        date:     String,
+        time:     String,
+        day:      String,
+        street:   String,
+        number:   String,
+        area:     String,
+        location: String
+    ) {
+        self.date     = date
+        self.time     = time
+        self.day      = day
+        self.street   = street
+        self.number   = number
+        self.area     = area
+        self.location = location
+    }
 }
 
 // output full time_range dictionary
@@ -124,6 +229,14 @@ public struct MailerAPIAvailabilityContent: Encodable {
 public struct MailerAPIDayAvailabilityContent: Codable {
     public let start: String
     public let end:   String
+
+    public init(
+        start:        String,
+        end:          String
+    ) {
+        self.start        = start
+        self.end          = end
+    }
 }
 
 public enum MailerAPIWeekday: String, RawRepresentable, CaseIterable, Identifiable, Encodable {
