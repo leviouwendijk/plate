ba5773b4a5add85a00ef3d2e5a3cacae13210b6c
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-05-21 13:12:35 +0200

    aligment in build info view

--- Sources/plate/views/build-information.swift
+++ Sources/plate/views/build-information.swift
@@ -1,19 +1,35 @@
 import SwiftUI
 
+public enum AlignmentStyle {
+    case leading
+    case trailing
+    case center
+}
+
 public struct BuildInformation: View {
     public let specification: BuildSpecification
+    public let alignment: AlignmentStyle
 
     public init(
-        specification: BuildSpecification
+        specification: BuildSpecification,
+        alignment: AlignmentStyle = .center
     ) {
         self.specification = specification
+        self.alignment = alignment
     }
     
     public var body: some View {
         HStack {
+            if alignment == .trailing {
+                Spacer()
+            }
+
             Text(specification.versionString())
                 .font(.footnote).foregroundColor(.secondary)
-            Spacer()
+
+            if alignment == .leading {
+                Spacer()
+            }
         }
         .padding(.horizontal)
         .padding(.vertical, 4)
