bafc955f54da87030d271c16c4c46bb5ddecb5d6
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-07-10 00:48:47 +0200

    exhaustive cases in formatting

--- Sources/plate/logic/date.swift
+++ Sources/plate/logic/date.swift
@@ -92,6 +92,7 @@ public enum DateParserFormatting: Sendable {
     case yyyyMMdd
     case ddMMyyyy
     case mmDDyyyy
+    case yyyyDDmm
     
     public func components(_ parts: [String]) throws -> DateComponents {
         let year: Int?
@@ -99,32 +100,41 @@ public enum DateParserFormatting: Sendable {
         let day: Int?
 
         switch self {
-            case .yyyyMMdd:
-                if parts[0].count == 4, parts[1].count == 2, parts[2].count == 2 {
-                    year  = Int(parts[0])
-                    month = Int(parts[1])
-                    day   = Int(parts[2])
-                } else {
-                    throw DateFormattingError.badStringForSelectedFormat
-                }
+        case .yyyyMMdd:
+            if parts[0].count == 4, parts[1].count == 2, parts[2].count == 2 {
+                year  = Int(parts[0])
+                month = Int(parts[1])
+                day   = Int(parts[2])
+            } else {
+                throw DateFormattingError.badStringForSelectedFormat
+            }
 
-            case .ddMMyyyy:
-                if parts[0].count == 2, parts[1].count == 2, parts[2].count == 4 {
-                    day   = Int(parts[0])
-                    month = Int(parts[1])
-                    year  = Int(parts[2])
-                } else {
-                    throw DateFormattingError.badStringForSelectedFormat
-                }
+        case .ddMMyyyy:
+            if parts[0].count == 2, parts[1].count == 2, parts[2].count == 4 {
+                day   = Int(parts[0])
+                month = Int(parts[1])
+                year  = Int(parts[2])
+            } else {
+                throw DateFormattingError.badStringForSelectedFormat
+            }
 
-            case .mmDDyyyy:
-                if parts[0].count == 2, parts[1].count == 2, parts[2].count == 4 {
-                    month   = Int(parts[0])
-                    day     = Int(parts[1])
-                    year    = Int(parts[2])
-                } else {
-                    throw DateFormattingError.badStringForSelectedFormat
-                }
+        case .mmDDyyyy:
+            if parts[0].count == 2, parts[1].count == 2, parts[2].count == 4 {
+                month   = Int(parts[0])
+                day     = Int(parts[1])
+                year    = Int(parts[2])
+            } else {
+                throw DateFormattingError.badStringForSelectedFormat
+            }
+
+        case .yyyyDDmm:
+            if parts[0].count == 4, parts[1].count == 2, parts[2].count == 2 {
+                year  = Int(parts[0])
+                day   = Int(parts[1])
+                month = Int(parts[2])
+            } else {
+                throw DateFormattingError.badStringForSelectedFormat
+            }
         }
 
         guard let y = year, let m = month, let d = day else {
