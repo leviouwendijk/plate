3d0ca98ee7ed4f789b5833897e7437a73d21a1f5
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-07-14 13:16:32 +0200

    customstringconvertible with newlines

---
+++ Sources/plate/logic/custom-string-convertible-with-newlines.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+extension Array where Element == CustomStringConvertible {
+    public func descriptionWithNewlines() -> String {
+        self.map { $0.description }.joined(separator: "\n")
+    }
+}
