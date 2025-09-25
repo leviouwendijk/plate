7e9a1eb0ea8d26c37d3ff8acda9319f56c71fb5e
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-09-25 19:44:32 +0200

    indentation printing

--- Sources/plate/logic/indent.swift
+++ Sources/plate/logic/indent.swift
@@ -1,11 +1,16 @@
 import Foundation
 
+public struct StandardIndentation: Sendable {
+    public static let size: Int = 4
+    public static let times: Int = 1
+}
+
 public protocol StringIndentable {
     func indent(_ indentation: Int, times: Int) -> String
 }
 
 extension String: StringIndentable {
-    public func indent(_ indentation: Int = 4, times: Int = 1) -> String { 
+    public func indent(_ indentation: Int = StandardIndentation.size, times: Int = StandardIndentation.times) -> String { 
         let spaces = indentation * times
         let indent = String(repeating: " ", count: spaces)
 
@@ -16,3 +21,38 @@ extension String: StringIndentable {
     }
 }
 
+// convenience func
+public func printi(_ s: String,_ indentation: Int = StandardIndentation.size, times: Int = StandardIndentation.times) -> Void { 
+    print(s.indent(indentation, times: times))
+}
+
+public func printindent(_ s: String,_ indentation: Int = StandardIndentation.size, times: Int = StandardIndentation.times) -> Void { 
+    printi(s, indentation, times: times)
+}
+
+public func prindent(_ s: String,_ indentation: Int = StandardIndentation.size, times: Int = StandardIndentation.times) -> Void { 
+    printi(s, indentation, times: times)
+}
+
+public func printdent(_ s: String,_ indentation: Int = StandardIndentation.size, times: Int = StandardIndentation.times) -> Void { 
+    printi(s, indentation, times: times)
+}
+
+// extension mirror
+extension String {
+    public func printi(_ indentation: Int = StandardIndentation.size, times: Int = StandardIndentation.times) -> Void { 
+        plate.printi(self, indentation, times: times)
+    }
+
+    public func printindent(_ indentation: Int = StandardIndentation.size, times: Int = StandardIndentation.times) -> Void { 
+        plate.printi(self, indentation, times: times)
+    }
+
+    public func prindent(_ indentation: Int = StandardIndentation.size, times: Int = StandardIndentation.times) -> Void { 
+        plate.printi(self, indentation, times: times)
+    }
+
+    public func printdent(_ indentation: Int = StandardIndentation.size, times: Int = StandardIndentation.times) -> Void { 
+        plate.printi(self, indentation, times: times)
+    }
+}
