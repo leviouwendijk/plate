c6bc8ef246be187c68a6d2060920f2dca3dcfae3
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-10-03 00:31:09 +0200

    type opt

--- Sources/plate/logic/project-structure.swift
+++ Sources/plate/logic/project-structure.swift
@@ -7,16 +7,16 @@ public enum ProjectPathSegmentType: Sendable {
 
 public protocol ProjectSegmentable: Sendable {
     var value: String { get set }
-    var type: ProjectPathSegmentType { get set }
+    var type: ProjectPathSegmentType? { get set }
 }
 
 public struct ProjectPathSegment: ProjectSegmentable {
     public var value: String
-    public var type: ProjectPathSegmentType
+    public var type: ProjectPathSegmentType?
     
     public init(
         value: String,
-        type: ProjectPathSegmentType
+        type: ProjectPathSegmentType?
     ) {
         self.value = value
         self.type = type
