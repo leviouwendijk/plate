a8bb635c8ad86ae878c0abc8e23b413b7170a8a5
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-10-06 12:16:48 +0200

    drop first / last ext improvements

--- Sources/plate/logic/trim.swift
+++ Sources/plate/logic/trim.swift
@@ -5,25 +5,71 @@ extension String {
         return self.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression)
     }
 
-    public func dropFirstWhitespace() -> String {
-        if self.hasPrefix(" ") { 
-            return String(self.dropFirst()) 
+    @inlinable
+    public func dropFirstOccurrence(of substring: String) -> String {
+        guard let range = range(of: substring) else { return self }
+        var copy = self
+        copy.removeSubrange(range)
+        return copy
+    }
+
+    @inlinable
+    public func dropLeading(_ string: String) -> String {
+        if self.hasPrefix(string) { 
+            return String(self.dropFirst(string.count)) 
         } else { 
             return self
         }
     }
 
-    public func dropLastWhitespace() -> String {
-        if self.hasSuffix(" ") { 
-            return String(self.dropFirst()) 
+    @inlinable
+    public func dropTrailing(_ string: String) -> String {
+        if self.hasSuffix(string) { 
+            return String(self.dropLast(string.count)) 
         } else { 
             return self
         }
     }
 
-    public func dropFirstAndLastWhitespace() -> String {
+    public func dropLeadingWhitespace() -> String {
+        return dropLeading(" ")
+    }
+
+    public func dropTrailingWhitespace() -> String {
+        return dropTrailing(" ")
+    }
+
+    public func dropLeadingNewline() -> String {
+        return dropLeading("\n")
+    }
+
+    public func dropTrailingNewline() -> String {
+        return dropTrailing("\n")
+    }
+
+    public func dropLeadingAndTrailing(_ string: String) -> String {
         return self
-            .dropFirstWhitespace()
-            .dropLastWhitespace()
+            .dropLeading(string)
+            .dropTrailing(string)
+    }
+
+    public func dropLeadingAndTrailingWhitespace() -> String {
+        let string = " "
+        return self
+            .dropLeading(string)
+            .dropTrailing(string)
+    }
+
+    public func dropLeadingAndTrailingNewline() -> String {
+        let string = "\n"
+        return self
+            .dropLeading(string)
+            .dropTrailing(string)
+    }
+
+    public func dropLeadingAndTrailingNewlineAndWhitespace() -> String {
+        return self
+            .dropLeadingAndTrailingNewline()
+            .dropLeadingAndTrailingWhitespace()
     }
 }
