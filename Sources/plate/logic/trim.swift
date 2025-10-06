66c760d9ffbf8dc7b76f821b53084b1f83419141
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-10-06 11:57:13 +0200

    drop first / last ext

--- Sources/plate/logic/trim.swift
+++ Sources/plate/logic/trim.swift
@@ -4,4 +4,26 @@ extension String {
     public func trimTrailing() -> String {
         return self.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression)
     }
+
+    public func dropFirstWhitespace() -> String {
+        if self.hasPrefix(" ") { 
+            return String(self.dropFirst()) 
+        } else { 
+            return self
+        }
+    }
+
+    public func dropLastWhitespace() -> String {
+        if self.hasSuffix(" ") { 
+            return String(self.dropFirst()) 
+        } else { 
+            return self
+        }
+    }
+
+    public func dropFirstAndLastWhitespace() -> String {
+        return self
+            .dropFirstWhitespace()
+            .dropLastWhitespace()
+    }
 }
