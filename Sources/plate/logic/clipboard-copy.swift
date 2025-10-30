6ec70c2576b310c35dc4141ac83ce1ac90a1d419
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-10-30 12:50:37 +0100

    updated copy func

--- Sources/plate/logic/clipboard-copy.swift
+++ Sources/plate/logic/clipboard-copy.swift
@@ -6,49 +6,107 @@ public enum CopyClipboardError: Error {
     case copyFailure(String)
 }
 
-// as function calls, backwards compatibility
+// // as function calls, backwards compatibility
+// public func copyToClipboard(_ text: String) {
+//     let pasteboard = NSPasteboard.general
+//     pasteboard.clearContents()
+//     pasteboard.setString(text, forType: .string)
+// }
+
+// // was the same as copyToClipboard, may need refactoring in places where this name called NSAttributed strings
+// public func copyNSToClipboard(_ attributedString: NSAttributedString) {
+//     let pasteboard = NSPasteboard.general
+//     pasteboard.clearContents()
+//     pasteboard.writeObjects([attributedString])
+// }
+
+// // now also as protocols
+// public protocol StringCopyable {
+//     func sCopy()
+// }
+
+// extension String: StringCopyable {
+//     public func sCopy() {
+//         let pasteboard = NSPasteboard.general
+//         pasteboard.clearContents()
+//         pasteboard.setString(self, forType: .string)
+
+//     }
+
+//     public func scp() {
+//         self.sCopy()
+//     }
+// }
+
+// public protocol NSAttributedStringCopyable {
+//     func nsCopy()
+// }
+
+// extension NSAttributedString: NSAttributedStringCopyable {
+//     public func nsCopy() {
+//         let pasteboard = NSPasteboard.general
+//         pasteboard.clearContents()
+//         pasteboard.writeObjects([self])
+
+//     }
+// }
+
+private extension NSPasteboard.PasteboardType {
+    static let utf8PlainText = NSPasteboard.PasteboardType("public.utf8-plain-text")
+}
+
 public func copyToClipboard(_ text: String) {
-    let pasteboard = NSPasteboard.general
-    pasteboard.clearContents()
-    pasteboard.setString(text, forType: .string)
+    let pb = NSPasteboard.general
+    pb.clearContents()
+    // Be explicit + multi-type (order: declare → set)
+    pb.declareTypes([.string, .utf8PlainText], owner: nil)
+    _ = pb.setString(text, forType: .string)
+    if let data = text.data(using: .utf8) {
+        _ = pb.setData(data, forType: .utf8PlainText)
+    }
 }
 
-// was the same as copyToClipboard, may need refactoring in places where this name called NSAttributed strings
 public func copyNSToClipboard(_ attributedString: NSAttributedString) {
-    let pasteboard = NSPasteboard.general
-    pasteboard.clearContents()
-    pasteboard.writeObjects([attributedString])
+    let pb = NSPasteboard.general
+    pb.clearContents()
+
+    // Write the attributed payload (RTF) *and* a plain-text fallback.
+    var declared: [NSPasteboard.PasteboardType] = []
+    if let rtf = try? attributedString.data(from: NSRange(location: 0, length: attributedString.length),
+                                            documentAttributes: [.documentType: NSAttributedString.DocumentType.rtf]) {
+        declared.append(.rtf)
+        pb.declareTypes([.rtf], owner: nil)
+        _ = pb.setData(rtf, forType: .rtf)
+    }
+
+    // Always also expose plain text for cli/nvim readers
+    let text = attributedString.string
+    if !declared.isEmpty {
+        // We already declared once; append additional types
+        _ = pb.setString(text, forType: .string)
+        if let data = text.data(using: .utf8) {
+            _ = pb.setData(data, forType: .utf8PlainText)
+        }
+    } else {
+        // Fallback path: declare plain text types
+        pb.declareTypes([.string, .utf8PlainText], owner: nil)
+        _ = pb.setString(text, forType: .string)
+        if let data = text.data(using: .utf8) {
+            _ = pb.setData(data, forType: .utf8PlainText)
+        }
+    }
 }
 
-// now also as protocols
-public protocol StringCopyable {
-    func sCopy()
-}
+public protocol StringCopyable { func sCopy() }
 
 extension String: StringCopyable {
-    public func sCopy() {
-        let pasteboard = NSPasteboard.general
-        pasteboard.clearContents()
-        pasteboard.setString(self, forType: .string)
-
-    }
-
-    public func scp() {
-        self.sCopy()
-    }
+    public func sCopy() { copyToClipboard(self) }
+    public func scp() { sCopy() }
 }
 
-public protocol NSAttributedStringCopyable {
-    func nsCopy()
-}
-
+public protocol NSAttributedStringCopyable { func nsCopy() }
 extension NSAttributedString: NSAttributedStringCopyable {
-    public func nsCopy() {
-        let pasteboard = NSPasteboard.general
-        pasteboard.clearContents()
-        pasteboard.writeObjects([self])
-
-    }
+    public func nsCopy() { copyNSToClipboard(self) }
 }
 
 public func copyFileObjectToClipboard(path file: String) throws {
