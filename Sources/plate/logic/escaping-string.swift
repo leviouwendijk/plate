a0e4570ad7fbaacd3bd80d81ee447475f1139cd1
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-11-13 22:10:10 +0100

    escape as .line default

--- Sources/plate/logic/escaping-string.swift
+++ Sources/plate/logic/escaping-string.swift
@@ -33,7 +33,7 @@ public enum EscapeKind {
 }
 
 extension String {
-    public func escape(_ kind: EscapeKind) -> String {
+    public func escape(_ kind: EscapeKind = .line) -> String {
         return EscapeKind.escape(self, kind: kind)
     }
 }
