2e108bccfeaae41f2c64f438bd666d237823bf21
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-11-14 22:27:13 +0100

    reversed precedence order

--- Sources/plate/logic/logger/log-level.swift
+++ Sources/plate/logic/logger/log-level.swift
@@ -1,23 +1,58 @@
 import Foundation
 
-public enum LogLevel: Int, Sendable, Codable, Comparable, CustomStringConvertible {
-    case info, warn, error, critical, debug
+public protocol Loggable: RawRepresentable, Comparable, Sendable, CaseIterable, Codable
+where RawValue == String {
+    var precedence: Int { get }
+    var label: String { get }
+}
 
-    public var label: String {
+public extension Loggable {
+    static func < (lhs: Self, rhs: Self) -> Bool {
+        // lhs.precedence < rhs.precedence
+        lhs.precedence > rhs.precedence
+    }
+}
+
+// public enum LogLevel: Int, Sendable, Codable, Comparable, CustomStringConvertible {
+//     case info, warn, error, critical, debug
+
+//     public var label: String {
+//         switch self {
+//         case .info:  return "INFO"
+//         case .warn:  return "WARN"
+//         case .error: return "ERROR"
+//         case .critical: return "CRITICAL"
+//         case .debug: return "DEBUG"
+//         }
+//     }
+
+//     public static func < (lhs: LogLevel, rhs: LogLevel) -> Bool {
+//         lhs.rawValue < rhs.rawValue
+//     }
+
+//     public var description: String {
+//         return self.label.lowercased()
+//     }
+// }
+
+public enum LogLevel: String, Loggable {
+    case debug 
+    case info 
+    case warn 
+    case error 
+    case critical 
+    
+    public var precedence: Int {
         switch self {
-        case .info:  return "INFO"
-        case .warn:  return "WARN"
-        case .error: return "ERROR"
-        case .critical: return "CRITICAL"
-        case .debug: return "DEBUG"
+        case .critical:  return 0
+        case .error:     return 1
+        case .warn:      return 2
+        case .info:      return 3
+        case .debug:     return 4
         }
     }
-
-    public static func < (lhs: LogLevel, rhs: LogLevel) -> Bool {
-        lhs.rawValue < rhs.rawValue
-    }
-
-    public var description: String {
-        return self.label.lowercased()
+    
+    public var label: String {
+        return self.rawValue.uppercased()
     }
 }
