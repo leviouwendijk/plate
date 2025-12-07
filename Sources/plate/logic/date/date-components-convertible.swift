e576a09f06c14a82958acf012cd7cf039a8a110b
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-12-07 10:40:34 +0100

    dateconvertible api rename

--- Sources/plate/logic/date/date-components-convertible.swift
+++ Sources/plate/logic/date/date-components-convertible.swift
@@ -5,7 +5,20 @@ public protocol DateConvertible {
 }
 
 extension DateComponents: DateConvertible { 
+    @available(*, message: "Deprecated, improved the same api to use just .date()")
     public func toDate(
+            using calendar: Calendar = .current,
+            _ timezone: CustomTimeZone = .amsterdam
+        ) throws -> Date {
+            var cal = calendar
+            cal.timeZone = try timezone.set()
+            guard let date = cal.date(from: self) else {
+                throw DateConversionError.invalidDateComponents
+            }
+        return date
+    }
+
+    public func date(
             using calendar: Calendar = .current,
             _ timezone: CustomTimeZone = .amsterdam
         ) throws -> Date {
