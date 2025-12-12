a5274b6e465f92741eb8446d0d6d271dfdd9ed81
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-12-12 11:50:00 +0100

    anysametype()

--- Sources/plate/logic/sametype.swift
+++ Sources/plate/logic/sametype.swift
@@ -3,3 +3,7 @@ import Foundation
 public func sametype<X, Y>(_ x: X.Type,_ y: Y.Type) -> Bool {
     return X.self == Y.self
 }
+
+public func anysametype(_ x: Any.Type,_ y: Any.Type) -> Bool {
+    return x.self == y.self
+}
