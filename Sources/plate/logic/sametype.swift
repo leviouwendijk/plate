e69d9dbd021ca2212939bd94fa781e84cbed8d9f
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-12-12 12:02:43 +0100

    verbose mode

--- Sources/plate/logic/sametype.swift
+++ Sources/plate/logic/sametype.swift
@@ -1,9 +1,29 @@
 import Foundation
 
-public func sametype<X, Y>(_ x: X.Type,_ y: Y.Type) -> Bool {
-    return X.self == Y.self
+public func sametype<X, Y>(
+    _ x: X.Type,
+    _ y: Y.Type,
+    verbose: Bool = false
+) -> Bool {
+    let result = (X.self == Y.self)
+    if verbose {
+        print("tYpe X ==", X.self)
+        print("tYpe Y ==", Y.self)
+        print("result:", result)
+    }
+    return result
 }
 
-public func anysametype(_ x: Any.Type,_ y: Any.Type) -> Bool {
-    return x.self == y.self
+public func anysametype(
+    _ x: Any.Type,
+    _ y: Any.Type,
+    verbose: Bool = false
+) -> Bool {
+    let result = (x.self == y.self)
+    if verbose {
+        print("type x ==", x.self)
+        print("type y ==", y.self)
+        print("result:", result)
+    }
+    return result
 }
