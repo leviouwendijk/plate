024f251567f374d953d9b3508b836075d60e92fc
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-12-16 23:42:39 +0100

    tld

--- Sources/plate/logic/url-protocol-component.swift
+++ Sources/plate/logic/url-protocol-component.swift
@@ -1,5 +1,14 @@
 import Foundation
 
+extension String {
+    public func replaceUnderscores(with string: String = ".") -> String {
+        return self.replacingOccurrences(
+            of: "_",
+            with: string
+        )
+    }
+}
+
 public protocol ProtocolComponent: Sendable, Codable, RawRepresentable where RawValue == String {
     var component: String { get }
 }
@@ -10,5 +19,29 @@ public enum HTTPProtocolComponent: String, ProtocolComponent {
 
     public var component: String {
         return self.rawValue + "://"
+    }
+}
+
+public enum TopLevelDomainComponent: String, ProtocolComponent {
+    case com
+    case net
+    case org
+    case edu
+    case gov
+    case info
+    case academy
+    case io
+
+    case nl
+    case de
+    case be
+    case uk
+    case il
+
+    case co_uk
+    case co_il
+
+    public var component: String {
+        return "." + self.rawValue.replaceUnderscores()
     }
 }
