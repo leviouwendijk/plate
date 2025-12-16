4f6269ae71ce0661eea5f027a6db65cbaba0d049
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-12-16 19:45:38 +0100

    daterange

---
+++ Sources/plate/logic/date-and-time/date-range.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+public struct DateRange: Sendable {
+    public let start: Date
+    public let end: Date
+    
+    public init(
+        start: Date,
+        end: Date
+    ) {
+        self.start = start
+        self.end = end
+    }
+
+    public init(using dates: (from: Date, to: Date)) {
+        self.start = dates.from
+        self.end = dates.to
+    }
+    
+    public init(from start: String, to end: String) throws {
+        let s = try start.date()
+        let e = try end.date()
+
+        self.init(using: (from: s, to: e))
+    }
+
+    public init(from start: String, interval: DateComponents) throws {
+        let s = try start.date()
+        let e = s + interval
+
+        self.init(using: (from: s, to: e))
+    }
+}
